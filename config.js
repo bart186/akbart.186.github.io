4921ce6a83713e41c5138379e344f808ab312f5c
bart186 <152833467+bart186@users.noreply.github.com>
2024-04-09 22:58:41 +0700

    Add files via upload
    Signed-off-by: bart186 <152833467+bart186@users.noreply.github.com>

---
+++ config.js
@@ -0,0 +1,22 @@
+// 霸都丶傲天 2019/7/9  https://Github.com/AJLoveChina
+var config = {
+    // 下面的句子不一定非要7句， 你也可以改成4句，2句话都可以
+    lines: [
+        "Sayangku cintaku liat ya",
+        "liat waktu dibawah kita udah selama ini bersama🥺💗",
+        "Ganyangka ya sayang bisa selama ini sama kamu tetep sayang sama cwo yg penuh kekurangan ini ya",
+        "Makasih telah lewatin bahagia likalikunya yg gaenak dan nerima semua sikap aku yg kurangnya🥺",
+        "Makasih uda terus berjuang sama sama lewatin hari demi hari",
+        "Aku minta maaf buat semua hal yg buat kamu terluka ya sayangg",
+        "Aku janji bakal berusaha lagi buat kamu ngerasa bahagia sama aku,dan pastinya mau terus sama sama ,sama kamu❤️‍🩹 ",
+    ],
+    // 相爱的时间 记住格式不能写错了, 非常重要
+    // 格式一定要和下面这个时间格式一样
+    // 分别是 年-月-日T时时:分分:秒秒
+    //      温馨警告:
+    //          1.时分秒都必须是俩个数字  比如 00:00:00
+    //          2.T不能丢了
+    date: "2023-05-20T20:15:00",
+    // 双方的名字
+    names: ["Makasih kebersamaannya", "Makasih buat semuanya sampai saat ini"]
+}
